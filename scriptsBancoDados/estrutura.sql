9520bc0d7d7419ca98be8188897c18c22365929a
Kaleo-Stark <kaleovieira.dev@gmail.com>
2022-06-30 03:04:08 -0300

    Iniciando front em Angular

--- scriptsBancoDados/estrutura.sql
+++ scriptsBancoDados/estrutura.sql
@@ -5,22 +5,22 @@ USE FECAF_sistema_vendas;
 CREATE TABLE IF NOT EXISTS produtos (
 	codigo        INT PRIMARY KEY AUTO_INCREMENT,
     descricao     VARCHAR(200) NOT NULL,
-    codigo_barras INT NOT NULL UNIQUE,
-    preco_atual   DECIMAL
+    codigo_barras VARCHAR(15) NOT NULL UNIQUE,
+    preco_atual   DECIMAL(6,2)
 );
 
 CREATE TABLE IF NOT EXISTS vendas (
 	codigo         INT PRIMARY KEY AUTO_INCREMENT,
     data_venda     DATETIME NOT NULL,
     tipo_pagamento VARCHAR(8) NOT NULL, -- CARTAO, DINHEIRO
-    valor_total    DECIMAL NOT NULL
+    valor_total    DECIMAL(6,2) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS produtos_venda (
 	codigo INT PRIMARY KEY AUTO_INCREMENT,
     codigo_venda INT,
     codigo_produto INT,
-    valor_produto_venda DECIMAL NOT NULL,
-    FOREIGN KEY (codigo_venda) REFERENCES venda(codigo),
+    valor_produto_venda DECIMAL(6,2) NOT NULL,
+    FOREIGN KEY (codigo_venda) REFERENCES vendas(codigo),
     FOREIGN KEY (codigo_produto) REFERENCES produtos(codigo)
 );
